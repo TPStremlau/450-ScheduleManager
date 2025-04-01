9ae497514f07107e082cdba37f80e310bf781676
Jackson Snyder <jps6302@uncw.edu>
2025-04-01 09:35:56 -0400

    added error correction

--- final_project/lib/home_page.dart
+++ final_project/lib/home_page.dart
@@ -1,12 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('u made it')),
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
+            )
+        ],
+      ),
+      body: Center(
+        child: Text("LOGGED IN AS: ${user.email!}",
+        style: TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
 }
